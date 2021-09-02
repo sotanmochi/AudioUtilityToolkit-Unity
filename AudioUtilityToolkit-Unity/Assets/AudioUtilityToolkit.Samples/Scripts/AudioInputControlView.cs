4effb3990eb5729566db51a250daec8a4a2188c1
sotanmochi <stmczk@gmail.com>
2021-09-02 22:14:21 +0900

    Add sample

---
+++ AudioUtilityToolkit-Unity/Assets/AudioUtilityToolkit.Samples/Scripts/AudioInputControlView.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace AudioUtilityToolkit.Samples
+{
+    public class AudioInputControlView : MonoBehaviour
+    {
+        [SerializeField] private Dropdown _selectDropdown;
+        [SerializeField] private Toggle _loopbackToggle;
+        
+        public IReadOnlyReactiveProperty<(int Index, string Name)> CurrentDevice => _currentItem;
+        private ReactiveProperty<(int Index, string Name)> _currentItem = new ReactiveProperty<(int Index, string Name)>();
+        
+        public IReadOnlyReactiveProperty<bool> LoopbackIsActive => _loopbackIsActive;
+        private ReactiveProperty<bool> _loopbackIsActive = new ReactiveProperty<bool>(true);
+        
+        private string[] _dropdownItems;
+        private string _dropdownMessage = "Select AudioInput";
+        
+        private void Awake()
+        {            
+            _selectDropdown.OnValueChangedAsObservable()
+            .Subscribe(selectedIndex => 
+            {
+                if (selectedIndex < 1)
+                {
+                    return;
+                }
+                int deviceIndex = selectedIndex - 1;
+                _currentItem.Value = (deviceIndex, _dropdownItems[deviceIndex]);
+            })
+            .AddTo(this);
+            
+            _loopbackToggle.OnValueChangedAsObservable()
+            .Subscribe(value => _loopbackIsActive.Value = value)
+            .AddTo(this);
+        }
+        
+        public void UpdateSelectDropdown(List<string> itemList)
+        {
+            _dropdownItems = itemList.ToArray();
+
+            _selectDropdown.ClearOptions();
+            _selectDropdown.RefreshShownValue();
+            _selectDropdown.options.Add(new Dropdown.OptionData { text = _dropdownMessage });
+            
+            foreach(var item in itemList)
+            {
+                _selectDropdown.options.Add(new Dropdown.OptionData { text = item });
+            }
+            
+            _selectDropdown.RefreshShownValue();
+        }
+    }
+}
---
+++ AudioUtilityToolkit-Unity/Assets/AudioUtilityToolkit.Samples/Scripts/AudioLoopbackPresenter.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UniRx;
+using AudioUtilityToolkit.SoundIOExtension;
+using AudioUtilityToolkit.UnityAudioExtension;
+
+namespace AudioUtilityToolkit.Samples
+{
+    public class AudioLoopbackPresenter : MonoBehaviour
+    {
+        [SerializeField] AudioInputControlView _audioInputControlView;
+        [SerializeField] UnityAudioOut _loopbackAudioOut;
+        
+        private UnityMicrophone _microphone;
+        private InputStream _inputStream;
+        
+        private void Awake()
+        {
+            // _microphone = new UnityMicrophone();
+            // _microphone.OnProcessFrame += OnProcessFrame;
+            // _audioInputControlView.UpdateSelectDropdown(Microphone.devices.ToList());
+
+            var inputDeviceList = AudioDeviceDriver.InputDeviceList.Select(device => device.DeviceName).ToList();
+            _audioInputControlView.UpdateSelectDropdown(inputDeviceList);
+
+            _audioInputControlView.CurrentDevice
+            .SkipLatestValueOnSubscribe()
+            .Subscribe(device => 
+            {
+                // _microphone.Stop();
+                // _microphone.Start(device.Name);
+                // _loopbackAudioOut.StartOutput();
+
+                _inputStream = AudioDeviceDriver.GetInputDevice(device.Name);
+                _inputStream.OnProcessFrame += OnProcessFrame;
+                _loopbackAudioOut.StartOutput(_inputStream.ChannelCount, _inputStream.SampleRate);
+            })
+            .AddTo(this);
+
+            _audioInputControlView.LoopbackIsActive
+            .SkipLatestValueOnSubscribe()
+            .Subscribe(loopback =>
+            {
+                if (loopback)
+                {
+                    _loopbackAudioOut.StartOutput();
+                }
+                else
+                {
+                    _loopbackAudioOut.StopOutput();
+                }
+            })
+            .AddTo(this);
+        }
+
+        private void OnDestroy()
+        {
+            // _microphone.OnProcessFrame -= OnProcessFrame;
+            // _microphone.Dispose();
+            _inputStream.OnProcessFrame -= OnProcessFrame;
+            _inputStream.Dispose();
+        }
+
+        private void OnProcessFrame(float[] data)
+        {
+            if (_audioInputControlView.LoopbackIsActive.Value)
+            {
+                _loopbackAudioOut.PushAudioFrame(data);
+            }
+        }
+    }
+}
